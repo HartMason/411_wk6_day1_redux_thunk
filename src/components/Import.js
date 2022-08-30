35e3b24139ef1d9e5716b4a8ecbae78c9b359837
HartMason <hartmason85@gmail.com>
2022-08-30 16:06:21 -0500

    added material ui component

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,11 +1,66 @@
-import React from 'react'
+import React from "react";
+// fill out this container
+import Button from "@mui/material/Button";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+// import { Container } from "@mui/system";
 
-const Import = (props) => {
-    // fill out this component
-
-    return (
-        <p>Import Component</p>
-    )
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
 }
 
-export default Import
+const rows = [
+//   createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+//   createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+//   createData("Eclair", 262, 16.0, 24, 6.0),
+//   createData("Cupcake", 305, 3.7, 67, 4.3),
+//   createData("Gingerbread", 356, 16.0, 49, 3.9),
+  
+];
+
+export default function BasicTable(props) {
+  return (
+    <TableContainer component={Paper}>
+        <Button onClick={props.fetchMakes}>Import</Button>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="right">Id</TableCell>
+            <TableCell align="right">Make</TableCell>
+            <TableCell align="right">Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.makes.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.MakeId}
+              </TableCell>
+              <TableCell align="right">{row.MakeName}</TableCell>
+              <TableCell align="right">{row.VehicleTypeName}</TableCell>
+              
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+// const Import = (props) => {
+//     // fill out this component
+
+//     return (
+//         <p>Import Component</p>
+//     )
+// }
+
+// export default Import
